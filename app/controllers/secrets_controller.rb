8dbb5a9eb9bff94fdc2e96d9f76ec7bfce529ec0
dlepperts <danielle.leppertsimenauer@gmail.com>
2021-01-22 15:07:09 -0600

    changes

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -2,19 +2,5 @@ class SecretsController < ApplicationController
     before_action :require_login
 
     def show
-        @secret = Secret.find(params[:id])
-    end
-
-    def index
-    end
-
-    def create
-        @secret = Secret.create(author_id: user_id)
-    end
-
-    private
-
-    def require_login
-        return head(:forbidden) unless session.includes? :user_id
     end
 end
